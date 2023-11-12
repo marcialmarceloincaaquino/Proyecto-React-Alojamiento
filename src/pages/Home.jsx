8d0fc27c03b1a4423973e6d24d4bf2606b7a1e5a
marcialmarceloincaaquino <marcialmarceloincaaquino@gmail.com>
2023-11-11 20:28:19 -0400

    ActualizacionfotoAndes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import Card from 'react-bootstrap/Card';
 import { Model } from '../Componentes/Cubo';
 import { Model1 } from '../Componentes/Cubo1';
 import { Model2 } from '../Componentes/Cubo2';
+import andesp from '/src/assets/imagenes/fotoAndeshd.png';
 
 //import "https://aframe.io/releases/1.4.0/aframe.min.js";
 
@@ -16,7 +17,7 @@ const Home = () => {
       <Container>
       <Row className='card text-bg-primary mb-3'><Col>**************************************************************</Col></Row>
         <Row>
-        <Col><img src="/src/assets/imagenes/fotoAndeshd.jpg" alt="" className='img-fluid max-width: 100% height: auto' /></Col>
+        <Col><img src={andesp} alt="" className='img-fluid max-width: 100% height: auto' /></Col>
         </Row>
 
       <Row>
